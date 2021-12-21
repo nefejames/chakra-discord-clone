3dfca05ea7a3302f7718e1746e58ca5e85643b5e
nefejames <nefejames@yahoo.com>
2021-12-21 09:26:20 +0000

    fixed channel link full-page reloading

--- components/ChannelLink.js
+++ components/ChannelLink.js
@@ -1,3 +1,4 @@
+import NextLink from "next/link";
 import { Box, Link, Text } from "@chakra-ui/react";
 import * as Icons from "@components/icons";
 import { useRouter } from "next/router";
@@ -13,31 +14,32 @@ export default function ChannelLink({
   const active = +channelId === +router.query.cid;
 
   return (
-    <Link
-      href={`/servers/1/channels/${channelId}`}
-      display="flex"
-      alignItems="center"
-      px="2"
-      mx="2"
-      py="1"
-      borderRadius="base"
-      bg={active && "gray.550"}
-      _hover={{ bg: "gray.550" }}
-      _focus={{ boxShadow: "none" }}
-    >
-      <Box mr="1.5" color="gray.400">
-        <Icon />
-      </Box>
-
-      <Text color={active && "white"} _hover={{ color: "gray.100" }} w="full">
-        {channelName}
-      </Text>
-
-      {secondIcon && (
-        <Box ml="auto" color="gray.200">
-          {secondIcon}
+    <NextLink href={`/servers/1/channels/${channelId}`} passHref>
+      <Link
+        display="flex"
+        alignItems="center"
+        px="2"
+        mx="2"
+        py="1"
+        borderRadius="base"
+        bg={active && "gray.550"}
+        _hover={{ bg: "gray.550" }}
+        _focus={{ boxShadow: "none" }}
+      >
+        <Box mr="1.5" color="gray.400">
+          <Icon />
         </Box>
-      )}
-    </Link>
+
+        <Text color={active && "white"} _hover={{ color: "gray.100" }} w="full">
+          {channelName}
+        </Text>
+
+        {secondIcon && (
+          <Box ml="auto" color="gray.200">
+            {secondIcon}
+          </Box>
+        )}
+      </Link>
+    </NextLink>
   );
 }
